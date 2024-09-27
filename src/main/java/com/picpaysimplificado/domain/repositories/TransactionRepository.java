21695de03818f68df03d2496949022675048b1ad
luangf <luangf222@gmail.com>
2024-09-26 23:29:51 -0300

    feat: create TransactionRepository

---
+++ src/main/java/com/picpaysimplificado/domain/repositories/TransactionRepository.java
@@ -0,0 +1,11 @@
+package com.picpaysimplificado.domain.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.picpaysimplificado.domain.transaction.Transaction;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+}
